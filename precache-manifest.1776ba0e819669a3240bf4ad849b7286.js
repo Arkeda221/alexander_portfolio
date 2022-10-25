28f65ec38ab758086e16f2f78ba469b8f3364324
Alexander Ab <ab.alexande21@gmail.com>
2022-10-25 17:31:46 -0400

    Updates

---
+++ precache-manifest.1776ba0e819669a3240bf4ad849b7286.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1b355be5175534edd4d98c1b12392c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36982011b1773d5a45b3",
+    "url": "/static/css/2.e45429e2.chunk.css"
+  },
+  {
+    "revision": "f37b552e38536a83860e",
+    "url": "/static/css/main.0e4b0296.chunk.css"
+  },
+  {
+    "revision": "36982011b1773d5a45b3",
+    "url": "/static/js/2.d593a677.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.d593a677.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f37b552e38536a83860e",
+    "url": "/static/js/main.20cc8b4d.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
